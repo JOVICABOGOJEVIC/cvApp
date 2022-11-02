ff81bf980f856a308cf37ac5ee9d94d96eb36e19
JOVICABOGOJEVIC <anastosic@Anas-MacBook-Pro.local>
2022-11-02 09:57:58 +0100

    fourthCommit

--- src/components/Players/PlayerList/PlayerList.js
+++ src/components/Players/PlayerList/PlayerList.js
@@ -3,6 +3,7 @@ import PlayersItem from '../PlayersItem/PlayersItem';
 import AddPlayers from '../AddPlayer/AddPlayer';
 import styles from './PlayerList.module.css';
 
+
 const PlayerList = () => {
     const [players, setData] = useState([]);
     const [message, setMessage] = useState();
@@ -11,50 +12,129 @@ const PlayerList = () => {
     const [card2, setCard2] = useState(0);
     const [goSecond, setGoSecond] = useState(false);
     const [card3, setCard3] = useState(0);
-    const [card4, setCard4] = useState();
-    const [card5, setCard5] = useState();
+    const [goThird, setGoThird] = useState(false);
+    const [card4, setCard4] = useState(0);
+    const [goForth, setGoForth] = useState(false);
+    const [card5, setCard5] = useState(0);
+    const [goFifth, setGoFifth] = useState(false);
+    const [card6, setCard6] = useState(0);
+    const [goSixth, setGoSixth] = useState(false);
+    const [card7, setCard7] = useState(0);
+    const [goSeventh, setGoSeventh] = useState(false);
+    const [view, setView] = useState(false);
+const [account,setAccount] =useState();
 
-let cardView = [];
-let cardTotal = card+card2+card3;
-console.log(card3);
-if(card3 === NaN){
-    card3 = 0;
-}
-cardView.push(cardTotal);
+    let cardView = [];
+    let cardTotal = card + card2 + card3 + card4 + card5 + card6 + card7;
+  
+  
+    cardView.push(cardTotal);
 
     const saveInfo = (ime, datum, razlika) => {
         setData((prevState) => {
             return [...prevState, { ime: ime, datum: datum, razlika: razlika, id: Math.random() }]
         })
     }
-    
-const startGame = () =>{
-    setMessage('Your first card is:');
-    setCard(Math.floor(Math.random() * 10) + 1);
-    setInterval(() => {
-setGoFirst(true);
-      }, 500);
-}
-const secondCard = ()=>{
-setCard2(Math.floor(Math.random() * 10) + 1);
-setInterval(() => {
-    setGoSecond(true);
-          }, 500);
-}
 
-
-const ThirdCard = ()=>{
-    setCard3(Math.floor(Math.random() * 10) + 1);
-    setInterval(() => {
-        setGoSecond(true);
-              }, 1500);
+    const startGame = () => {
+        setMessage('Your first card is:');
+        setCard(Math.floor(Math.random() * 10) + 1);
+        setInterval(() => {
+            setGoFirst(true);
+        }, 500);
     }
-    
-const quitGame = () =>{
+  
+    const secondCard = (event) => {
+        setCard2(Math.floor(Math.random() * 10) + 1);
+            setGoSecond(true);
+            event.currentTarget.disabled = true;
+    }
 
+
+    const thirdCard = (event) => {
+        if(cardTotal>21){
+            setView(true)
+        }
+        setCard3(Math.floor(Math.random() * 10) + 1);
+            setGoThird(true);
+            event.currentTarget.disabled = true;
+    }
+    const forthCard = (event) => {
+        if(cardTotal>21){
+            setView(true)
+        }
+        setCard4(Math.floor(Math.random() * 10) + 1);
+            setGoForth(true);
+            event.currentTarget.disabled = true;
+    }
+    const fifthCard = (event) => {
+        if(cardTotal>21){
+            setView(true)
+        }
+        setCard5(Math.floor(Math.random() * 10) + 1);
+            setGoFifth(true);
+            event.currentTarget.disabled = true;
+    }
+    const sixthCard = (event) => {
+        if(cardTotal>21){
+            setView(true)
+        }
+        setCard6(Math.floor(Math.random() * 10) + 1);
+            setGoSixth(true);
+            event.currentTarget.disabled = true;
+    }
+    const seventhCard = (event) => {
+        if(cardTotal>21){
+            setView(true)
+        }
+        setCard7(Math.floor(Math.random() * 10) + 1);
+            setGoSeventh(true);
+            event.currentTarget.disabled = true;
+    }
+    const quitGame = () => {
+        console.log('radi');
+      setView(true)
+    }
+
+
+return (
+        <Fragment>
+            <div className={styles.container}>
+                <h4>Cards game</h4>
+                <AddPlayers onSavePlayers={saveInfo} newPlay={startGame} />
+
+                <h4>Not over 21 in total:</h4>
+                <PlayersItem playersInfo={players} />Your account<span>{account-10}$</span>
+                <div className={`${!view && styles.view} ${view && styles.notView}`}>
+                {message}<br />
+                <p className={styles.card}>{card}</p>
+                {goFirst && (<div><button onClick={secondCard}>Continue?</button><button onClick={quitGame}>Quit?</button></div>)}
+
+                <p className={`${!goSecond && styles.wait} ${goSecond && styles.going} ${styles.card}`}>{card2} </p><br />
+                {goSecond && (<div><button onClick={thirdCard}>Continue?</button></div>)}
+                <p className={`${!goThird && styles.wait} ${goThird && styles.going} ${styles.card}`}>{card3} </p><br />
+                {goThird && (<div><button onClick={forthCard}>Continue?</button></div>)}
+                <p className={`${!goForth && styles.wait} ${goForth && styles.going} ${styles.card}`}>{card4} </p><br />
+                {goForth && (<div><button onClick={fifthCard}>Continue?</button></div>)}
+                <p className={`${!goFifth && styles.wait} ${goFifth && styles.going} ${styles.card}`}>{card5} </p><br />
+                {goFifth && (<div><button onClick={sixthCard}>Continue?</button></div>)}
+                <p className={`${!goSixth && styles.wait} ${goSixth && styles.going} ${styles.card}`}>{card6} </p><br />
+                {goSixth && (<div><button onClick={seventhCard}>Continue?</button></div>)}
+                <p className={`${!goSeventh && styles.wait} ${goSeventh && styles.going} ${styles.card}`}>{card7} </p><br />
+                {goSecond && (<p className={`${styles.total}`}>{cardView} </p>)}
+                </div>
+                <div className={`${!view && styles.report}`}>
+                    <p>Izvestaj</p>
+                    <p>Rezultat:{cardView}</p>
+                    <div>
+                    </div>
+                    </div>
+            </div>
+
+        </Fragment>
+    )
 }
-    return (
-        <div className={styles.container}>
+{/*<div className={styles.container}>
             <h4>Cards game</h4>
             <AddPlayers onSavePlayers={saveInfo} newPlay={startGame}/>
             <h4>Not over 21 in total:</h4>
@@ -71,8 +151,7 @@ const quitGame = () =>{
          <p className={`${!goSecond && styles.wait} ${goSecond && styles.going} ${styles.card}`}>{card3} </p><br/>
          <hr/>
          <p className={`${!goFirst && styles.wait} ${goFirst && styles.going} ${styles.total}`}>{cardView} </p>
-        </div>
-    )
-}
+    </div>*/}
+
 
 export default PlayerList;
